9c8079034f0df29aa9174bde23483a3e1c499380
singhsonu-dot <singhsonu89860@gmail.com>
2026-06-16 13:42:09 +0530

    fix search bar text layout

--- src/components/ui/SearchBar.jsx
+++ src/components/ui/SearchBar.jsx
@@ -1,7 +1,7 @@
 function SearchBar({ value, onChange, placehholder }) {
     return (
-        <div className="mb-4 flex flex-col gap-2">
-            <label htmlFor="search" className="text-sm font-medium text-gray-600">Search Bar</label>
+        <div className="mb-4 flex flex-col text-black gap-2">
+            <label htmlFor="search" className="text-sm font-medium text-gray-600"/> 
 
             <input id="search" type="text" value={value} onChange={onChange} placeholder={placehholder}/>
         </div>
